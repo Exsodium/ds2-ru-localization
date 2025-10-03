2430883a3704e207e1ed6e5cd2d92521a857edf7
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-03 20:58:09 +0300

    Add stopwatch

--- write_all_fmg_to_csv.py
+++ write_all_fmg_to_csv.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from fmg.writer import FmgWriter
+from time import time
 
 EXT = '*.fmg'
 
@@ -8,5 +9,10 @@ russian_path = Path(r'original_text\text\russian')
 runglish_files = list(zip(english_path.rglob(EXT), russian_path.rglob(EXT)))
 
 fmg_writer = FmgWriter()
+start_time = time()
+
 for english_file, russian_file in runglish_files:
     fmg_writer.write_fmg_files_to_csv(english_file, russian_file)
+
+end_time = time()
+print(f'{round(end_time - start_time, 3)} с.')
