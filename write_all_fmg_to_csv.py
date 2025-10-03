0fa94689a0f0d2241faaeb04296485d795652e8f
Alexey Lazarev <lexalazarev2002@yandex.ru>
2025-10-03 21:06:45 +0300

    Remove stopwatch

--- write_all_fmg_to_csv.py
+++ write_all_fmg_to_csv.py
@@ -9,10 +9,5 @@ russian_path = Path(r'original_text\text\russian')
 runglish_files = list(zip(english_path.rglob(EXT), russian_path.rglob(EXT)))
 
 fmg_writer = FmgWriter()
-start_time = time()
-
 for english_file, russian_file in runglish_files:
     fmg_writer.write_fmg_files_to_csv(english_file, russian_file)
-
-end_time = time()
-print(f'{round(end_time - start_time, 3)} с.')
